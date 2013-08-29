420e3ece20770a0150514f251709f8d1f7e3afa9
Eduardo Otubo <eduardo.otubo@gmail.com>
2013-08-29 20:06:01 -0300

    WWWW: trying to come up with a great mechanic
    Trying to come up with a great mechanic for jumping and collision on
    walls.

--- WWWW/Assets/Standard Assets/Scripts/HeroControl.js
+++ WWWW/Assets/Standard Assets/Scripts/HeroControl.js
@@ -1,8 +1,8 @@
 @script RequireComponent(AudioSource)
 
-var speed : float = 6.0;
-var jumpSpeed : float = 1.0;
-var gravity : float = 1.1;
+var speed : float = 0.1;
+var jumpSpeed : float = 0.4;
+var gravity : float = 2.0;
 private var jumping : boolean = false;
 
 var scene_number : int;
@@ -12,11 +12,21 @@ var MAXY : float;
 
 function Start ()
 {
+	rigidbody.drag = 0;
+	if (Physics.gravity != Vector3(0,-20,0))
+		Physics.gravity = Vector3(0,-20,0);
+
+	rigidbody.useGravity = true;
 }
 
 function OnCollisionEnter(collision : Collision){
-	if(collision.gameObject.name == "floor"){
+	if (collision.gameObject.name == "floor"){
 		jumping = false;
+		//rigidbody.constraints = RigidbodyConstraints.FreezePositionY.
+		//rigidbody.useGravity = false;
+	}
+	if (collision.gameObject.name == "wall"){
+		rigidbody.velocity = Vector3(0,0,0);
 	}
 }
 
@@ -25,17 +35,29 @@ function Update () {
 	var posicaoX : float;
 	posicaoX = transform.position.x;
 	
-	if (posicaoX > MAXX){
-		Application.LoadLevel(scene_number);
+//	if (posicaoX > MAXX){
+//		Application.LoadLevel(scene_number);
+//	}
+
+	if (Input.GetKey(KeyCode.RightArrow)){
+		transform.position.x += speed;
+		//rigidbody.AddForce(speed,0,0);
+	}
+	
+	if (Input.GetKey(KeyCode.LeftArrow)){
+		transform.position.x -= speed;
+		//rigidbody.AddForce(-speed,0,0);
 	}
 
-	if (Input.GetAxis("Horizontal")){
-		transform.Translate(Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0));
-	}
+	//if (Input.GetAxis("Horizontal")){
+	//	transform.Translate(Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0));
+	//}
 	
 	if (Input.GetKeyDown(KeyCode.Space) && jumping == false){
 		audio.Play();
 		jumping = true;
+    	//Physics.gravity *= -1;
+    	//rigidbody.useGravity = true;
 		rigidbody.velocity.y = -jumpSpeed * Physics.gravity.y;	
 	}
 }
